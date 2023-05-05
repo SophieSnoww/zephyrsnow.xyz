efd39208cf62ed6a311259855156032fe0820f1a
Sophie Snow <zephyr.snow@pm.me>
2023-05-05 16:19:41 -0600

    Changed stuff on homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,13 +13,18 @@
     <div class="window large">
       <div class="window-title">~/home.txt</div>
       <h1>About.Me</h1>
-      <p>I'm Zephyr, a red white and black deer, and I use any pronouns. I spend my time on plenty of things, including programming, making music, playing games, and editing videos.</p>
+      <p>I'm Zephyr, and I use any pronouns. I spend my time on a lotta different things, including programming, making music, playing games, and editing videos.</p>
       <br /><br />
       <h1>Stuff.I.Program</h1>
       <p>I do a lot of web development, and I have experience with both front-end and back-end. I've written plenty of Discord bots, and random unfinished miscellanious projects - browser extensions, small video games, or mods for Minecraft and Terraria. My <a href="https://github.com/ZephyrSnoww" target="_blank">Github</a> has a few of these projects.</p>
       <br /><br />
       <h1>Games.I.Play</h1>
-      <p>I play Beat Saber, OSU!Standard & OSU!Mania, Valorant, CS:GO, Minecraft, and Terraria to name a few. I love horror, but I get frightened incredibly easily, so I basically never play horror games.</p>
+      <p>I play TF2, Beat Saber, OSU!Standard & OSU!Mania, Valorant, CS:GO, Minecraft, and Terraria to name a few.</p>
+      <br /><br />
+      <h1>Main.Links</h1>
+      <p>
+        My main, active socials/links you might be interested in are my <a href="https://discord.gg/AmqvmdMYWE">Discord</a>, <a href="https://www.twitch.tv/zephyrsnoww">Twitch</a>, and <a href="https://www.youtube.com/@zephyr_ii">YouTube</a>.
+      </p>
     </div>
   </div>
 </template>
